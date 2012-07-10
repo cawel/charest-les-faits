918975c1c3dce886788e35339b2de1ed39873d3b
Matt <matt@matthias>
2012-07-10 20:33:03 +0200

    translated routes to french

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,7 @@ CharestLaHonte::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  # translate urls to french
+  ActionDispatch::Routing::Translator.translate_from_file('config/locales/routes.yml')
 end
