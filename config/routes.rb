2a199771afa23c757af4aef5321acaf5a5acafd9
Martin Carel <martin.carel@gmail.com>
2012-07-10 15:50:44 -0400

    fixed fr mistake

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ CharestLaHonte::Application.routes.draw do
   #   end
 
   match 'a-propos' => 'application#about', :as => 'about'
-  match 'se-debarasser-de-la-honte' => 'application#good_riddance', :as => 'good_riddance'
+  match 'se-debarrasser-de-la-honte' => 'application#good_riddance', :as => 'good_riddance'
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'application#index'
