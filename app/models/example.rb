c461afbea2a09f51405b95f1f50cf7e6b703977d
Martin Carel <martin.carel@gmail.com>
2012-07-10 18:59:13 -0400

    example: first pass on the 'new' form

--- app/models/example.rb
+++ app/models/example.rb
@@ -1,4 +1,8 @@
 class Example < ActiveRecord::Base
   attr_accessible :excerpt, :headline, :link, :reasons
   has_and_belongs_to_many :reasons
+
+  validates_presence_of :headline
+  validates_presence_of :excerpt
+  validates_presence_of :link
 end
