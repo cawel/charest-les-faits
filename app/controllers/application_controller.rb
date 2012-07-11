528fa55660a2e8778196cfe7637066944b37205a
Martin Carel <martin.carel@gmail.com>
2012-07-10 22:59:46 -0400

    slideshow: improved randomizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
-     random_id = rand(Example.count)
-     @example = Example.first(:conditions => [ "id >= ?", random_id])
-     @example = Example.new({:headline => "Ceci est un exemple bidon"}) if @example.nil?
+    @example = Example.all.sample
   end
 
   private
