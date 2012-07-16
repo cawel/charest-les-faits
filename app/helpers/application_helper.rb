8d40f28d4b37efbaf6689d2e5d97f117044acf5e
Martin Carel <martin.carel@gmail.com>
2012-07-16 12:56:57 -0400

    toggling excerpt source with style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def current_reason? reason
     session[:filter_by_reason] == reason.id.to_s
   end
+
+  def extract_website link
+    no_http = link.sub(/^http:\/\//,'').sub(/[w.]*/,'')
+    no_slashes = no_http.sub(/\/.*/, '')
+  end
 end
