3ec0598ba4bffe237054b119f84ba3857bff07d6
Matt <matt@matthias>
2012-08-11 17:48:56 +0200

    added social network buttons to blog posts and made the posts path french

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ CharestLaHonte::Application.routes.draw do
   match 'exemples/confirmation' => 'examples#create_confirm', :as => :create_confirm
   match 'exemples/top-10-les-plus-partages' => 'examples#top', :as => :top
 
-  resources :posts, :only => [:show]
+  resources :posts, :path => 'billets', :only => [:show]
 
   resources :examples, :only => [:new, :create, :show],
     :path => "exemples",
@@ -17,7 +17,7 @@ CharestLaHonte::Application.routes.draw do
     match 'shares/trigger_fetch' => 'shares#trigger_fetch', :as => :trigger_fetch
     resources :examples
     resources :reasons
-    resources :posts, :except => :show
+    resources :posts, :path => 'billets', :except => :show
   end
 
   match 'a-propos' => 'application#about', :as => 'about'
